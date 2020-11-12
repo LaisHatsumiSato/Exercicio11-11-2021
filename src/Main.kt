984d866621de7420308e07db38d3a217dcd47e40
Lais Hatsumi Sato <laishsat@hotmail.com>
2020-11-11 22:05:37 -0300

    adicionando variaveis

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,28 @@
 fun main() {
-    var aluno = Aluno("123456", "alex", )
-println()
+    var aluno = Aluno(12345, "alex", "silva")
+    var aluno1 = Aluno(65432, "bbb", "DDD")
+    var aluno2 = Aluno(98765, "ccc", "FFF")
+
+    var materia = Materia("mobile")
+    var materia1 = Materia("python")
+    var materia2 = Materia("java")
+
+    var aula = Aula(materia, "09:00", "12:00")
+    var aula1 = Aula(materia1, "10:00", "14:00")
+    var aula2 = Aula(materia2, "11:00", "13:00")
+
+    var professor = Docente (1234,"ffff", "hhhhh")
+
+
+    var listaAula = ArrayList<Aula>()
+    listaAula.add(aula)
+    listaAula.add(aula1)
+    listaAula.add(aula2)
+    var listaAluno = ArrayList<Aluno>()
+    listaAluno.add(aluno)
+    listaAluno.add(aluno1)
+    listaAluno.add(aluno2)
+
+    println()
+
 }
